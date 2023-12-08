bb4c23e86efe3d65fb729f266920932d4e51794e
mrnazu <samuelamsalu1998@gmail.com>
2023-12-08 13:23:32 +0300

    Define variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""Define variables"""
-
+""" Define variables """
 
 a: int = 1
 pi: float = 3.14
-i_undersand_annotations: bool = True
+i_understand_annotations: bool = True
 school: str = "Holberton"
