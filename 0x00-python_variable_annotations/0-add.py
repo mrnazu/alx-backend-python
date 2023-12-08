1499000b8dd8ee1071e9c2652ac5027e1246baaf
mrnazu <samuelamsalu1998@gmail.com>
2023-12-08 11:55:53 +0300

    0. Basic annotations - add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python3
+"""This is a brief description of the module."""
+
 def add(a: float, b: float) -> float:
-    """Add two floating-point numbers and return the result."""
+    """
+    Add two floating-point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
     result = a + b
     return result
 
